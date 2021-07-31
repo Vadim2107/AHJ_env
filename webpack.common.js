22d9a5e9b1167e2c8246ed0bbd52811291ddb013
Vadim2107 <58651870+Vadim2107@users.noreply.github.com>
2021-07-31 17:48:07 +0300

    add files webpack

---
+++ webpack.common.js
@@ -0,0 +1,72 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/js/app.js',
+  mode: 'production',
+  stats: {
+    children: true,
+  },
+  // output: {
+  //   path: path.resolve(__dirname, 'dist'),
+  // },
+  output: {
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+        ],
+      },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/i,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.svg$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(png|jpg|gif|webp)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 8192, // будет использоваться file-loader
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+};
